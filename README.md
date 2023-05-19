147762dd593abd3d432aac7704e9c66aadb759fb
fachinmax <maxfachin98@gmail.com>
2023-05-19 17:34:33 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# Frontend Mentor - News homepage
+
+Challenge by Frontend Mentor
+
+## Caratteristiche
+
+Sito web fluid responsive
+
+# Strumenti
+
+Per la realizzazione del sito sono stati utilizzati i linguaggi:
+
+- HTML
+- CSS
+- Javascript
+
+è stata utilizzata la metodologia CUBE CSS, è stata utilizzato SCSS come preprocessore, Parcel come web application bundler e sono stati utilizzati gli strumenti messi a disposizione da [Utopia](https://utopia.fyi/) per realizzare il sito in maniera fluid.
---
+++ src/js/main.js
@@ -0,0 +1,19 @@
+// get elements
+let btnToggleNavbar = document.querySelector('#btn-navbar')
+let navbar = document.querySelector('.navbar')
+
+btnToggleNavbar.addEventListener('click', event => {
+    let btn = event.target
+    let ariaExpanded = btnToggleNavbar.getAttribute('aria-expanded')
+    if(ariaExpanded === 'true') {
+        btn.setAttribute('aria-expanded', 'false')
+        btn.setAttribute('data-icon', 'open')
+        navbar.setAttribute('data-visible', 'false')
+        document.body.setAttribute('data-background', 'bright')
+    } else {
+        btn.setAttribute('aria-expanded', 'true')
+        btn.setAttribute('data-icon', 'close')
+        navbar.setAttribute('data-visible', 'true')
+        document.body.setAttribute('data-background', 'dark')
+    }
+})
